bc5c073718df594b9d151f51548d49be5f337695
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-28 00:10:28 +0900

    feat : 환경변수 방식 변경

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -1,15 +1,19 @@
+import * as dotenv from 'dotenv';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as config from 'config';
 
-const dbConfig = config.get('db');
+dotenv.config();
+
+const stringToBoolean = (value: string): boolean => {
+  return value.toLowerCase() === 'true';
+};
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: dbConfig.type,
-  host: process.env.RDS_HOSTNAME || dbConfig.host,
-  port: process.env.RDS_PORT || dbConfig.port,
-  username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.RDS_PASSWORD || dbConfig.password,
-  database: process.env.RDS_DB_NAME || dbConfig.database,
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '3306', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: dbConfig.synchronize,
+  synchronize: stringToBoolean(process.env.DB_SYNCHRONIZE),
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as config from 'config';
-
-const serverConfig = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = serverConfig.port;
+  const port = process.env.SERVER_PORT;
 
   await app.listen(port);
 }
