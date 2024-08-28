d2a8ebd4b6b542d33635217bccea6460aa1f28c7
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-28 17:34:09 +0900

    chore : 디렉토리 수정

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
+import { UsersController } from './controller/users.controller';
+import { UsersService } from 'src/users/application/users.service';
+import { UserRepository } from './domain/user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './domain/user.entity';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UsersController],
+  providers: [UsersService, UserRepository],
 })
 export class UsersModule {}
