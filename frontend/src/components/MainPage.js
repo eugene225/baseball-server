6c5811e27730527d67882f62469ae2cdfc89dcee
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-28 16:40:56 +0900

    feat - frontend : 메인페이지 재구성

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,22 +1,45 @@
+// src/components/MainPage.js
 import React from 'react';
-import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
 import './MainPage.css';
 
 function MainPage() {
-  const teams = [
-    'team1.png', 'team2.png', 'team3.png', 'team4.png', 'team5.png',
-    'team6.png', 'team7.png', 'team8.png', 'team9.png', 'team10.png'
-  ];
-
   return (
-    <div>
-      <Navbar />
-      <div className="main-content">
-        <div className="logo-grid">
-          {teams.map((team, index) => (
-            <img key={index} src={`/assets/teamLogos/${team}`} alt={`Team ${index + 1}`} className="team-logo" />
-          ))}
-        </div>
+    <div className="main-page">
+      <h1>⚾️ Let's BaseBall ⚾️</h1>
+      <div className="auth-buttons">
+        <Link to="/login" className="auth-button">로그인</Link>
+        <Link to="/signup" className="auth-button">회원가입</Link>
+      </div>
+      <div className="board-container">
+        <Link to="/diary" className="board-item">
+          <div className="board-content">
+            <span className="emoji">📓</span>
+            <h2>야구 일기</h2>
+            <p>나만의 야구 일기를 기록하세요.</p>
+          </div>
+        </Link>
+        <Link to="/log" className="board-item">
+          <div className="board-content">
+            <span className="emoji">📅</span>
+            <h2>직관 Log</h2>
+            <p>직관한 경기의 정보를 기록합니다.</p>
+          </div>
+        </Link>
+        <Link to="/teams" className="board-item">
+          <div className="board-content">
+            <span className="emoji">🏆</span>
+            <h2>실시간 팀 현황</h2>
+            <p>팀의 현재 상태를 확인하세요.</p>
+          </div>
+        </Link>
+        <Link to="/chat" className="board-item">
+          <div className="board-content">
+            <span className="emoji">💬</span>
+            <h2>채팅방</h2>
+            <p>다른 팬들과 소통하세요.</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
