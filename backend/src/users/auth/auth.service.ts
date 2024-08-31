1e4eb9868f48997dee1350ef7f60ce8242d228cc
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-31 14:27:54 +0900

    feat : user login 구현

--- backend/src/users/auth/auth.service.ts
+++ backend/src/users/auth/auth.service.ts
@@ -3,6 +3,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { SignUpRequestDto } from '../dto/signup-request.dto';
 import { UserDto } from '../dto/user.dto';
+import { SignInRequestDto } from '../dto/signIn-request.dto';
+import { SignInResponseDto } from '../dto/signIn-response.dto';
+import * as bcrypt from 'bcryptjs';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class AuthService {
   constructor(
@@ -14,5 +18,18 @@ export class AuthService {
   async signUp(signUpRequestDto: SignUpRequestDto): Promise<UserDto> {
     const user = await this.userRepository.createUser(signUpRequestDto);
     return UserDto.create(user);
+  }
+
+  async singIn(signInRequestDto: SignInRequestDto): Promise<SignInResponseDto> {
+    const { email, password } = signInRequestDto;
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { email };
+      const accessToken = await this.jwtService.sign(payload);
+      return SignInResponseDto.create(email, accessToken);
+    } else {
+      throw new UnauthorizedException('login failed - email {}', email);
+    }
   }
 }
