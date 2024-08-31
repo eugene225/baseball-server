0b14de797086bf079e339ccca74f26a78e488101
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-31 14:13:59 +0900

    feat : user domain 구성

--- backend/src/users/domain/user.repository.ts
+++ backend/src/users/domain/user.repository.ts
@@ -1,8 +1,32 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { SignUpRequestDto } from '../dto/signup-request.dto';
+import * as bcrypt from 'bcryptjs';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export class UserRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(singUpRequestDto: SignUpRequestDto): Promise<User> {
+    const { nickname, email, password } = singUpRequestDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ nickname, email, password: hashedPassword });
+
+    try {
+      return await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing Email');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
