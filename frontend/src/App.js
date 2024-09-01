3def053ce4397f681793a82f7a48e2758c130ec1
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-01 18:50:46 +0900

    feat - frontend : mypage api 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
+import MyPage from './components/MyPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
