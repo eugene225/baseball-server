998061f8ceab0d7041ade7c9faa88f3e822e2e42
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-07 02:46:13 +0900

    frontend - fix : data구조 수정 및 디자인 수정

--- frontend/src/api/diary.js
+++ frontend/src/api/diary.js
@@ -6,7 +6,7 @@ const API_URL = 'http://52.65.47.31:3000/api/v1/diary';
 export const fetchPublicDiaries = async () => {
   try {
     const response = await axios.get(`${API_URL}/public`);
-    return response.data.data; // 데이터 구조 확인 후 수정
+    return response.data;
   } catch (error) {
     console.error('Failed to fetch public diaries:', error);
     throw new Error(error.response ? error.response.data.message : '공개 일기장 목록을 가져오는 데 실패했습니다.');
