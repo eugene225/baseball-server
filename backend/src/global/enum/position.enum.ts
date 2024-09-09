b6153192104b8428331da4d12f0ef1b9fcf15dd6
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-10 01:36:36 +0900

    backend - feat : position enum 변경

--- backend/src/global/enum/position.enum.ts
+++ backend/src/global/enum/position.enum.ts
@@ -1,12 +1,14 @@
 export enum Position {
-    STARTING_PITCHER = 'SP',
-    CATCHER = 'C',
-    FIRST_BASE = '1B',
-    SECOND_BASE = '2B',
-    THIRD_BASE = '3B',
-    SHORTSTOP = 'SS',
-    LEFT_FIELD = 'LF',
-    CENTER_FIELD = 'CF',
-    RIGHT_FIELD = 'RF',
-    DESIGNATED_HITTER = 'DH'
+    PITCHER = 'P',         // 투수
+    CATCHER = 'C',         // 포수
+    FIRST_BASE = '1B',     // 1루수
+    SECOND_BASE = '2B',    // 2루수
+    THIRD_BASE = '3B',     // 3루수
+    SHORTSTOP = 'SS',      // 유격수
+    LEFT_FIELD = 'LF',     // 좌익수
+    CENTER_FIELD = 'CF',   // 중견수
+    RIGHT_FIELD = 'RF',    // 우익수
+    DESIGNATED_HITTER = 'DH', // 지명 타자
+    INFIELDER = 'INF',     // 내야수
+    OUTFIELDER = 'OUT'     // 외야수
 }
