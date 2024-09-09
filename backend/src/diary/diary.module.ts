c70bc24ac99ec560dee8c2ac749938ec19235e34
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-09 23:17:24 +0900

    backend - feat : diary module 설정 추가

--- backend/src/diary/diary.module.ts
+++ backend/src/diary/diary.module.ts
@@ -7,13 +7,16 @@ import { DiaryService } from './application/diary.service';
 import { UserService } from 'src/users/application/user.service';
 import { UserRepository } from 'src/users/domain/user.repository';
 import { PassportModule } from '@nestjs/passport';
+import { DiaryEntry } from './domain/diaryEntry.entity';
+import { DiaryEntryRepository } from './domain/diaryEntry.repository';
+import { DiaryEntryService } from './application/diaryEntry.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Diary]),
+    TypeOrmModule.forFeature([Diary, DiaryEntry]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   controllers: [DiaryController],
-  providers: [DiaryRepository, UserRepository, DiaryService, UserService],
+  providers: [DiaryRepository, UserRepository, DiaryService, UserService, DiaryEntryRepository, DiaryEntryService],
 })
 export class DiaryModule {}
