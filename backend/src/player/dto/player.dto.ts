1ac4876775aaf6b124af9aaf712be6ee10f688e0
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-11 18:31:20 +0900

    backend - chore : eslint 정리

--- backend/src/player/dto/player.dto.ts
+++ backend/src/player/dto/player.dto.ts
@@ -1,4 +1,3 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Position } from 'src/global/enum/position.enum';
 import { Team } from 'src/global/enum/team.enum';
 import { Player } from '../domain/player.entity';
@@ -8,15 +7,10 @@ export class PlayerDto {
     public readonly id: number,
     public readonly name: string,
     public readonly position: Position,
-    public readonly team: Team
+    public readonly team: Team,
   ) {}
 
   static fromPlayer(player: Player): PlayerDto {
-    return new PlayerDto(
-      player.id,
-      player.name,
-      player.position,
-      player.team
-    );
+    return new PlayerDto(player.id, player.name, player.position, player.team);
   }
 }
