08045cfe960a22950c4f673d507088a6d26f11c9
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-12 00:44:50 +0900

    backend - chore : eslint 적용

--- backend/src/diary/domain/diary-entry.repository.ts
+++ backend/src/diary/domain/diary-entry.repository.ts
@@ -1,11 +1,10 @@
-import { CreateDiaryEntryRequestDto } from './../dto/create-diary-entry-request.dto';
-import { DataSource, Repository } from "typeorm";
-import { DiaryEntry } from "./diary-entry.entity";
-import { CustomRepository } from "src/global/decorator/custom-repository.decorator";
+import { DataSource, Repository } from 'typeorm';
+import { DiaryEntry } from './diary-entry.entity';
+import { CustomRepository } from 'src/global/decorator/custom-repository.decorator';
 
 @CustomRepository(DiaryEntry)
 export class DiaryEntryRepository extends Repository<DiaryEntry> {
-    constructor(dataSource: DataSource) {
-        super(DiaryEntry, dataSource.createEntityManager());
-    }
+  constructor(dataSource: DataSource) {
+    super(DiaryEntry, dataSource.createEntityManager());
+  }
 }
