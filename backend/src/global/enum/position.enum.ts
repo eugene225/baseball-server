44d0b902acc2f2277d14f83523acc1c0cae9eaa2
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-12 02:43:00 +0900

    backend - chore : eslint

--- backend/src/global/enum/position.enum.ts
+++ backend/src/global/enum/position.enum.ts
@@ -1,14 +1,14 @@
 export enum Position {
-    PITCHER = 'P',         // 투수
-    CATCHER = 'C',         // 포수
-    FIRST_BASE = '1B',     // 1루수
-    SECOND_BASE = '2B',    // 2루수
-    THIRD_BASE = '3B',     // 3루수
-    SHORTSTOP = 'SS',      // 유격수
-    LEFT_FIELD = 'LF',     // 좌익수
-    CENTER_FIELD = 'CF',   // 중견수
-    RIGHT_FIELD = 'RF',    // 우익수
-    DESIGNATED_HITTER = 'DH', // 지명 타자
-    INFIELDER = 'INF',     // 내야수
-    OUTFIELDER = 'OUT'     // 외야수
+  PITCHER = 'P', // 투수
+  CATCHER = 'C', // 포수
+  FIRST_BASE = '1B', // 1루수
+  SECOND_BASE = '2B', // 2루수
+  THIRD_BASE = '3B', // 3루수
+  SHORTSTOP = 'SS', // 유격수
+  LEFT_FIELD = 'LF', // 좌익수
+  CENTER_FIELD = 'CF', // 중견수
+  RIGHT_FIELD = 'RF', // 우익수
+  DESIGNATED_HITTER = 'DH', // 지명 타자
+  INFIELDER = 'INF', // 내야수
+  OUTFIELDER = 'OUT', // 외야수
 }
