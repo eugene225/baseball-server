61fcd1d620283775134746482caafabb50ea21c4
eugene <81062608+eugene225@users.noreply.github.com>
2024-09-12 00:31:09 +0900

    frontend - api : diary api 수정

--- frontend/src/api/diary.ts
+++ frontend/src/api/diary.ts
@@ -1,21 +1,28 @@
 import axios, { AxiosError } from 'axios';
 import { ErrorResponse } from '../types/global';
-import { CreateDiaryData, Diary } from '../types/diary';
+import { CreateDiaryData, Diary, DiaryCardContent, DiaryEntry } from '../types/diary';
 
 // API URL 상수
-const API_URL = 'http://52.65.47.31:3000/api/v1/diary';
+const API_URL = 'http://52.65.47.31:3000/api/v1/diarys';
 
 // 공개 일기장 목록을 가져오는 API 호출 함수의 반환 타입 정의
-export const fetchPublicDiaries = async (): Promise<Diary[]> => {
+export const fetchPublicDiaries = async (token: string): Promise<Diary[]> => {
   try {
-    const response = await axios.get<Diary[]>(`${API_URL}/public`);
+    // 요청 헤더에 인증 토큰 추가
+    const response = await axios.get<Diary[]>(`${API_URL}/public`, {
+      headers: {
+        Authorization: `Bearer ${token}`, // Bearer 토큰을 헤더에 추가
+      },
+    });
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       const axiosError = error as AxiosError<ErrorResponse>;
       const errorMessage = axiosError.response?.data?.message || '공개 일기장 목록을 가져오는 데 실패했습니다.';
+      console.error('API 호출 에러:', errorMessage); // 에러 메시지 로깅
       throw new Error(errorMessage);
     } else {
+      console.error('API 호출 에러:', error);
       throw new Error('공개 일기장 목록을 가져오는 데 실패했습니다.');
     }
   }
@@ -38,4 +45,9 @@ export const createDiary = async (diaryData: CreateDiaryData, accessToken: strin
       throw new Error('일기장을 생성하는 데 실패했습니다.');
     }
   }
+};
+
+export const fetchDiaryEntries = async (diaryId: number): Promise<DiaryEntry[]> => {
+  const response = await axios.get<DiaryEntry[]>(`${API_URL}/${diaryId}`);
+  return response.data;
 };
