2ad82934118baff68c7140c1d76c1d963b2a7a2a
eugene <81062608+eugene225@users.noreply.github.com>
2025-04-07 12:40:13 +0900

    backend - fix : 404 error

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -14,14 +14,13 @@ app.add_middleware(
     max_age=3600,
 )
 
-@app.get("/ai/kbo/rank")
-def read_kbo_rank():
-    return get_kbo_rank()
-
-@app.get("/ai")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/kbo/rank")
+def read_kbo_rank():
+    return get_kbo_rank()
 
 
 
