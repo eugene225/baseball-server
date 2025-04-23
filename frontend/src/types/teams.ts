dd16be13fc6b963571225ecd1913053cdc6f6bba
eugene <81062608+eugene225@users.noreply.github.com>
2025-04-23 15:23:23 +0900

    frontend - type : team 별 상징색 추가

--- frontend/src/types/teams.ts
+++ frontend/src/types/teams.ts
@@ -2,19 +2,21 @@
 export interface Team {
     value: string;
     label: string;
+    color: string;
 }
 
 export const TEAMS: Team[] = [
-  { value: 'LG_TWINS', label: 'LG 트윈스' },
-  { value: 'SAMSUNG_LIONS', label: '삼성 라이온즈' },
-  { value: 'KIWOOM_HEROS', label: '키움 히어로즈' },
-  { value: 'HANHWA_EAGLES', label: '한화 이글스' },
-  { value: 'KT_WIZ', label: 'KT 위즈' },
-  { value: 'DOOSAN_BEARS', label: '두산 베어스' },
-  { value: 'NC_DINOS', label: 'NC 다이노스' },
-  { value: 'SSG_LANDERS', label: 'SSG 랜더스' },
-  { value: 'KIA_TIGERS', label: 'KIA 타이거즈' },
+  { value: 'LG_TWINS', label: 'LG 트윈스', color: '#C30452' },
+  { value: 'SAMSUNG_LIONS', label: '삼성 라이온즈', color: '#1D2088' },
+  { value: 'KIWOOM_HEROS', label: '키움 히어로즈', color: '#76232F' },
+  { value: 'HANHWA_EAGLES', label: '한화 이글스', color: '#EF7C00' },
+  { value: 'KT_WIZ', label: 'KT 위즈', color: '#231F20' },
+  { value: 'DOOSAN_BEARS', label: '두산 베어스', color: '#0C2340' },
+  { value: 'NC_DINOS', label: 'NC 다이노스', color: '#1D4E89' },
+  { value: 'SSG_LANDERS', label: 'SSG 랜더스', color: '#E61A20' },
+  { value: 'KIA_TIGERS', label: 'KIA 타이거즈', color: '#E60012' },
 ];
+
 
 export enum Stadium {
   CHANGWON_NC_PARK = '창원NC파크',
