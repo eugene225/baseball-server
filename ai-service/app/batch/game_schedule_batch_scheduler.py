90e319ade25eadb93bc5a545e6352d2645a2c170
eugene <81062608+eugene225@users.noreply.github.com>
2025-04-30 17:52:35 +0900

    backend - fix : scheduler db 커넥션 관리 비동기 처리 제거

--- ai-service/app/batch/game_schedule_batch_scheduler.py
+++ ai-service/app/batch/game_schedule_batch_scheduler.py
@@ -1,19 +1,18 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.executors.asyncio import AsyncIOExecutor
 from datetime import datetime
 from app.schedule.service.game_schedule_service import GameScheduleService
 from app.database.db import get_db
-import asyncio
 import logging
 
 logging.basicConfig(level=logging.INFO)
 scheduler = AsyncIOScheduler(executors={"default": AsyncIOExecutor()})
 
+
 async def save_schedule_task():
     logging.info("스케줄 저장 작업 시작")
     try:
-        async with get_db() as db:
+        with get_db() as db:
             today = datetime.today()
             service = GameScheduleService(db)
             result = await service.save_game_schedule(today.year, today.month)
@@ -22,10 +21,14 @@ async def save_schedule_task():
         logging.error(f"스케줄 저장 중 오류 발생: {e}")
     logging.info("스케줄 저장 작업 완료")
 
-def start_schedule_task():
-    @scheduler.scheduled_job('cron', hour=0, minute=0)
-    def scheduled_job():
-        logging.info("스케줄러 작업 실행")
-        asyncio.run(save_schedule_task())
 
+async def scheduled_job():
+    logging.info("스케줄러 작업 실행")
+    await save_schedule_task()
+
+
+def start_schedule_task():
+    @scheduler.scheduled_job('cron', hour=17, minute=46)
+    async def job():
+        await scheduled_job()
     scheduler.start()
