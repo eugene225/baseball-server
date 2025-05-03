cb7eb3c30cf59bccc0d353b46af328c5ec0f0000
eugene <81062608+eugene225@users.noreply.github.com>
2025-05-04 00:08:25 +0900

    Update firebase-admin.ts

--- backend/src/configs/firebase-admin.ts
+++ backend/src/configs/firebase-admin.ts
@@ -2,14 +2,16 @@ import admin from 'firebase-admin';
 import * as fs from 'fs';
 
 const rawData = fs.readFileSync(new URL('../../src/configs/serviceAccountKey.json', import.meta.url), 'utf-8');
-
 const serviceAccount = JSON.parse(rawData);
 
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const credential = {
+  ...serviceAccount,
+  private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(credential as admin.ServiceAccount),
   });
 }
 
