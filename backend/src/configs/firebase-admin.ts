3311ca1dff8c0648d9b757483439f2244b097b07
eugene <81062608+eugene225@users.noreply.github.com>
2025-05-03 23:52:34 +0900

    backend - fix : private_key 파싱 문제 수정

--- backend/src/configs/firebase-admin.ts
+++ backend/src/configs/firebase-admin.ts
@@ -2,7 +2,10 @@ import admin from 'firebase-admin';
 import * as fs from 'fs';
 
 const rawData = fs.readFileSync(new URL('../../src/configs/serviceAccountKey.json', import.meta.url), 'utf-8');
+
 const serviceAccount = JSON.parse(rawData);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
   admin.initializeApp({
