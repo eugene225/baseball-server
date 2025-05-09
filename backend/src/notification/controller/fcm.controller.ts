bb80ffd7f702e59e7b41517a5579ce5a13e30aa9
eugene <81062608+eugene225@users.noreply.github.com>
2025-05-10 00:27:05 +0900

    backend - feat : fcm token get api 추가

--- backend/src/notification/controller/fcm.controller.ts
+++ backend/src/notification/controller/fcm.controller.ts
@@ -1,5 +1,5 @@
 import { FcmService } from '../application/fcm.service.js';
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('/v1/fcm')
 export class FcmController {
@@ -12,6 +12,11 @@ export class FcmController {
     @Body('fcmToken') fcmToken: string,
   ) {
     await this.fcmService.updateFcmToken(userId, fcmToken);
+  }
+
+  @Get('/:userId/fcm-token')
+  async getFcmToken(@Param('userId') userId: number) {
+    return await this.fcmService.getFcmToken(userId);
   }
 
   @Delete('/:userId/fcm-token')
