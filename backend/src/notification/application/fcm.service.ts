5bf1dec1956e9b5124a360a5ddf20b509951f850
eugene <81062608+eugene225@users.noreply.github.com>
2025-05-10 00:29:38 +0900

    backend - feat : fcm token CRUD 메서드 구현 / multicast 방식 알림 전송 메서드 구현

--- backend/src/notification/application/fcm.service.ts
+++ backend/src/notification/application/fcm.service.ts
@@ -14,36 +14,73 @@ export class FcmService {
     return await this.fcmInfoRepository.findAllFcmTokenIsNotNull();
   }
 
+  async getFcmToken(userId: number) {
+    return await this.fcmInfoRepository.findByUserId(userId);
+  }
+
   async updateFcmToken(userId: number, fcmToken: string) {
-    const fcmInfo = await this.fcmInfoRepository.findByUserId(userId);
-    await this.fcmInfoRepository.updateByUserId(userId, fcmToken);
+    try {
+      await this.fcmInfoRepository.updateByUserId(userId, fcmToken);
+      console.log('FCM 토큰 DB 저장 완료');
+    } catch (error) {
+      console.error('FCM 토큰 저장 실패:', error);
+      throw error;
+    }
   }
 
   async deleteFcmToken(userId: number) {
-    await this.fcmInfoRepository.updateByUserId(userId, null);
+    try {
+      await this.fcmInfoRepository.updateByUserId(userId, null);
+      console.log('FCM 토큰 DB 삭제 완료');
+    } catch (error) {
+      console.error('FCM 토큰 삭제 실패:', error);
+      throw error;
+    }
   }
 
-  async sendNotificationToUser(userId: number, title: string, body: string) {
-    const fcmInfo = await this.fcmInfoRepository.findByUserId(userId);
-    if (!fcmInfo || !fcmInfo.fcmToken) {
-      console.log("FCM 토큰이 없어 알림을 전송할 수 없습니다.");
+  async sendMulticastNotification(tokens: string[], title: string, body: string) {
+    if (!tokens || tokens.length === 0) {
+      console.log("전송할 FCM 토큰이 없습니다.");
+      return;
     }
 
     const message = {
-      token: fcmInfo.fcmToken,
       notification: {
         title,
         body,
       },
+      tokens,
     };
-
+  
     try {
-      const response = await admin.messaging().send(message);
-      console.log('알림 전송 성공:', response);
+      const response = await admin.messaging().sendEachForMulticast(message);
+      console.log(`알림 전송 결과:`, {
+        total: tokens.length,
+        success: response.successCount,
+        failure: response.failureCount
+      });
+  
+      const failedTokens: string[] = [];
+  
+      response.responses.forEach((resp, idx) => {
+        if (!resp.success) {
+          const failedToken = tokens[idx];
+          console.error(`토큰 전송 실패:`, {
+            token: failedToken,
+            error: resp.error,
+            code: resp.error?.code,
+            details: resp.error?.message
+          });
+          failedTokens.push(failedToken);
+        }
+      });
+      
+      if (failedTokens.length > 0) {
+        await this.fcmInfoRepository.deleteTokens(failedTokens);
+      }
     } catch (err) {
-      console.error('알림 전송 실패:', err);
+      console.error('멀티캐스트 전송 실패:', err);
       throw err;
     }
   }
-
 }
