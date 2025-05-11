857c2f9f4e94e75caf99b33a88509595b0b94367
eugene <81062608+eugene225@users.noreply.github.com>
2025-05-11 19:48:51 +0900

    frontend - api : 수정된 api 적용

--- frontend/src/api/fcm.ts
+++ frontend/src/api/fcm.ts
@@ -1,27 +1,32 @@
 
 const API_URL = `${ process.env.REACT_APP_API_URL }/v1/fcm`;
 
-export const saveFcmToken = async (userId: string, token: string, fcmToken: string) => {
-  return fetch(`${API_URL}/${userId}/fcm-token`, {
+export const saveFcmToken = async (userId: string, token: string, fcmToken: string, deviceType: string) => {
+  return fetch(`${API_URL}/token`, {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ fcmToken }),
+    body: JSON.stringify({
+      userId,
+      fcmToken,
+      deviceType
+    }),
   });
 };
 
-export const getFcmToken = async (userId: string, token: string) => {
-  return fetch(`${API_URL}/${userId}/fcm-token`, {
+export const getFcmToken = async (userId: string, token: string, deviceType: string) => {
+  return fetch(`${API_URL}/token?userId=${userId}&deviceType=${deviceType}`, {
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 };
 
-export const deleteFcmToken = async (userId: string, token: string) => {
-  return fetch(`${API_URL}/${userId}/fcm-token`, {
+export const deleteFcmToken = async (userId: string, token: string, deviceType: string) => {
+  return fetch(`${API_URL}/token?userId=${userId}&deviceType=${deviceType}`, {
     method: 'DELETE',
     headers: {
       Authorization: `Bearer ${token}`,
