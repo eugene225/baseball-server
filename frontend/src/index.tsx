c997771e63f908ed91790d644166e64d7a98cd28
eugene <81062608+eugene225@users.noreply.github.com>
2025-05-16 19:36:57 +0900

    frontend - chore : strict mode 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,9 +9,9 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    //<React.StrictMode>
+    <App />
+    //</React.StrictMode>
   );
 } else {
   console.error('Root element not found');
